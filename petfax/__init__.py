e552c51bb41f11d1d8a6769c0898ca75722d25bb
Brenton Ring <97139815+brentonring@users.noreply.github.com>
2022-08-29 19:31:26 -0600

    facts index up and running

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
